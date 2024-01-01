cdc6af818f496b74606720c6058e47b420ac31e0
Anirudha Bhurke <20708087+anirudhabhurke@users.noreply.github.com>
2024-01-01 13:58:52 +0530

    Update stats.py

--- stats.py
+++ stats.py
@@ -5,6 +5,7 @@ from time import sleep
 import subprocess
 from PIL import ImageFont
 from pathlib import Path
+import sys
 
 serial = i2c(port=1, address=0x3C)
 device = sh1106(serial)
@@ -17,22 +18,29 @@ def make_font(name, size):
 
 font = make_font("FiraCode-Regular.ttf", 11)
 
-while True:
-    with canvas(device) as draw:
-        cmd = "hostname -I | cut -d' ' -f1"
-        IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
-        cmd = 'cut -f 1 -d " " /proc/loadavg'
-        CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
-        cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB\", $3,$2 }'"
-        MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
-        cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
-        Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
-        ip_text = "IP: " + IP
-        cpu_text = "CPU load: " + CPU
-        mem_text = MemUsage
-        disk_text = Disk
-        draw.text((0, 0), ip_text, fill="white", font=font)
-        draw.text((0, 15), cpu_text, fill="white", font=font)
-        draw.text((0, 30), mem_text, fill="white", font=font)
-        draw.text((0, 45), disk_text, fill="white", font=font)
-    sleep(5)
+
+def display_stats():
+    while True:
+        with canvas(device) as draw:
+            cmd = "hostname -I | cut -d' ' -f1"
+            IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
+            cmd = 'cut -f 1 -d " " /proc/loadavg'
+            CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
+            cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB\", $3,$2 }'"
+            MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
+            cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
+            Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
+            ip_text = "IP: " + IP
+            cpu_text = "CPU load: " + CPU
+            draw.text((0, 0), ip_text, fill="white", font=font)
+            draw.text((0, 15), cpu_text, fill="white", font=font)
+            draw.text((0, 30), MemUsage, fill="white", font=font)
+            draw.text((0, 45), Disk, fill="white", font=font)
+        sleep(5)
+
+
+try:
+    display_stats()
+except KeyboardInterrupt:
+    # Graceful exit code here
+    sys.exit(0)
